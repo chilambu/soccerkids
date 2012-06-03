64033b3217649d9d9c0b2b54b08ec309525bd57a
chilambarasan <chilambu.it@gmail.com>
2012-06-03 20:44:08 +0530

    commit secnd

--- app/controllers/payments_controller.rb
+++
@@ -1,6 +0,0 @@
-class PaymentsController < ApplicationController
-	#before_filter :authenticate_user!
-	#prepend_before_filter:authenticate_user!
-	def hi
-		end
-end
--- db/migrate/20120523112554_add_userid_to_player.rb
+++
@@ -1,9 +0,0 @@
-class AddUseridToPlayer < ActiveRecord::Migration
-  def self.up
-    add_column :players, :user_id, :integer
-  end
-
-  def self.down
-    remove_column :players, :user_id
-  end
-end
